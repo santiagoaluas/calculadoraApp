4edda1a3779d9f37bb0256df8401857c57910e42
Santiago Alulema <salulema@sistec.com.ec>
2021-07-28 19:13:24 -0500

    Completo
    Completo calculadora

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,126 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  mensajes: string;
+  public numeroConcatenar: string;
+  operacion ="";
+  a ="";
+  b ="";
+  c ="";
+  d ="";  
+  e ="";
+  ceContador = 0
+  constructor(public toastController: ToastController) {
+    this.operacion='';
+  }
 
+
+  entervalue(value:string){
+    if ((this.b =="+") || (this.b=="-") || (this.b=="*") || (this.b=="/") || (this.b=="%")){
+      this.d=this.d+value;
+      this.operacion = this.operacion+value;
+      this.c=this.d;
+    }else{
+      this.operacion = this.operacion+value;
+      this.a= this.operacion;
+    }
+  }
+  condition(value:string){
+    this.operacion = this.operacion+value;
+    console.log("condition" + this.operacion)
+
+    this.b =value;
+    console.log("" + this.b)
+  }
+
+
+  clear(){
+    this.operacion ="";
+    this.a ="";
+    this.b ="";
+    this.c ="";
+    this.d ="";  
+    this.e ="";
+  }
+
+  Ce(){
+    if(this.operacion !== ""){
+      this.operacion = this.operacion.split(this.b)[0] + this.b
+    }
+    
+  }
+
+  divicionParados(){
+    if(this.operacion !== ""){
+      this.operacion = (parseFloat(this.operacion)/2).toString()
+    }
+  }
+
+  raizCuadrada(){
+    if(this.operacion !== ""){
+      this.operacion =  Math.sqrt(parseFloat(this.operacion)).toString()
+    }
+  }
+
+  cuadradonumero(){
+    if(this.operacion !== ""){
+      this.operacion =  Math.pow(parseFloat(this.operacion),2).toString()
+    }
+  }
+
+  eliminarUltimo(){
+    if(this.operacion !== ""){
+      
+        const texto : string = this.operacion.toString()
+        this.operacion = texto.substring(0, texto.length - 1);
+    }
+  }
+
+   objectoArray: any[] = [];
+  
+  result(){
+    if (this.b == "+"){
+      this.operacion = `${this.operacion} = ${(parseFloat(this.a)+parseFloat(this.c)).toString()}`;
+      this.operacion = (parseFloat(this.operacion)+parseFloat(this.c)).toString();
+    
+      this.objectoArray.push(parseFloat(this.operacion)+parseFloat(this.c));
+      console.log(this.objectoArray);
+    }
+    if (this.b == "%"){
+      this.operacion = `${this.operacion} = ${(parseFloat(this.a)%parseFloat(this.c)).toString()}`;
+      console.log(parseFloat(this.operacion));
+      console.log(parseFloat(this.c));
+
+      this.operacion = (parseFloat(this.operacion)%parseFloat(this.c)).toString();
+    
+      this.objectoArray.push(parseFloat(this.operacion)%parseFloat(this.c));
+    }
+    if (this.b == "-"){
+      this.operacion = `${this.operacion} = ${(parseFloat(this.a)-parseFloat(this.c)).toString()}`;
+      this.operacion = (parseFloat(this.operacion)-parseFloat(this.c)).toString();
+    
+     // this.objectoArray.push(parseFloat(this.operacion)+parseFloat(this.c));
+     // console.log(this.objectoArray);
+    }
+    if (this.b == "*"){
+      this.operacion = `${this.operacion} = ${(parseFloat(this.a)*parseFloat(this.c)).toString()}`;
+      this.operacion = (parseFloat(this.operacion)*parseFloat(this.c)).toString();
+    
+     // this.objectoArray.push(parseFloat(this.operacion)+parseFloat(this.c));
+     // console.log(this.objectoArray);
+    }
+    if (this.b == "/"){
+      if (this.c == "0"){
+        this.operacion = "no existe division para cero"
+      }else{
+        this.operacion = `${this.operacion} = ${(parseFloat(this.a)/parseFloat(this.c)).toString()}`;
+        this.operacion = (parseFloat(this.operacion)/parseFloat(this.c)).toString();
+      }
+     
+    
+     // this.objectoArray.push(parseFloat(this.operacion)+parseFloat(this.c));
+     // console.log(this.objectoArray);
+    }
+  
+}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  imgurl;
+  constructor(private camera: Camera) {}
+  getGaleria(){
 
-  constructor() {}
-
+  }
+  getCamera(){
+  
+  }
 }
